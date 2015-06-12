2fd512bb0f0540fb50b8b7f86d0596be4a60ccd7
Mike Hollis <mikey.tennis@gmail.com>
2015-06-12 00:07:29 -0400

    Update procfile and puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,10 @@ threads 0,8
 workers 2
 preload_app!
 
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
