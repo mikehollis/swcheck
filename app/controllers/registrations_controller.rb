24f645a4ef7751d70152bce3082d01c5c38730a4
Mike Hollis <mikey.tennis@gmail.com>
2017-06-25 11:23:47 -0400

    Remove registration ability

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    flash[:notice] = "Sorry, but registration is closed"
+    redirect_to :root
+  end
   protected
 
   def after_sign_up_path_for(resource)
